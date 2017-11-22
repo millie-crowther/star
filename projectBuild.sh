2d894bbe6528639f8d1d5fdfb5bf8446be497e4b
millie <mbc15@ic.ac.uk>
2017-11-22 00:28:18 +0000

    added more messages to project builder

--- projectBuild.sh
+++ projectBuild.sh
@@ -1,4 +1,50 @@
 cd build
+
+echo "+-------------------+"
+echo "| building makefile |"
+echo "+-------------------+"
 cmake CMakeLists.txt .
+if [ $? -ne 0 ]; then
+  echo "+------------------------------------+"
+  echo "| Error building makefile. Aborting. |"
+  echo "+------------------------------------+"
+  exit
+else
+  echo "+-----------------------------+"
+  echo "| makefile built successfully |"
+  echo "+-----------------------------+"
+fi
+
+echo ""
+
+echo "+------------------+"
+echo "| building project |"
+echo "+------------------+"
 make
+if [ $? -ne 0 ]; then
+  echo "+-----------------------------------+"
+  echo "| Error building project. Aborting. |"
+  echo "+-----------------------------------+"
+  exit
+else
+  echo "+----------------------------+"
+  echo "| project built successfully |"
+  echo "+----------------------------+"
+fi
+echo ""
+
+echo "+-----------+"
+echo "| executing |"
+echo "+-----------+"
 ./star
+result=$?
+if [ $result -ne 0 ]; then
+  echo "+-------------------------------------+"
+  echo "| program quit abnormally with code $result |"
+  echo "+-------------------------------------+"
+  exit
+else
+  echo "+---------------------------+"
+  echo "| program quit successfully |"
+  echo "+---------------------------+"
+fi
--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ int main(){
   engine_t engine;
   engine.run();
 
+
   sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
