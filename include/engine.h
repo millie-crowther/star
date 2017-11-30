ca2e7ba322f11954b32c7f37196e0066763f2109
millie <mbc15@ic.ac.uk>
2017-11-30 21:26:00 +0000

    opengl works now, small test program running

--- include/engine.h
+++ include/engine.h
@@ -1,10 +1,13 @@
 #ifndef STAR_ENGINE_H
 #define STAR_ENGINE_H
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 class engine_t {
 private:
+    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLint mvp_location, vpos_location, vcol_location;
     GLFWwindow * window;
 
     void draw();
--- src/engine.cpp
+++ src/engine.cpp
@@ -1,5 +1,36 @@
 #include "engine.h"
 #include <iostream>
+#include "glm/glm.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+
+static const struct
+{
+    float x, y;
+    float r, g, b;
+} vertices[3] =
+{
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
+    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+};
+
+static const char* vertex_shader_text =
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+
+static const char* fragment_shader_text =
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
 
 static void 
 key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
@@ -24,6 +55,33 @@ engine_t::initialise(){
     glfwMakeContextCurrent(window);
 
     glfwSetKeyCallback(window, key_callback);
+    
+    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+
+
+    glfwSwapInterval(1);
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+    mvp_location = glGetUniformLocation(program, "MVP");
+    vpos_location = glGetAttribLocation(program, "vPos");
+    vcol_location = glGetAttribLocation(program, "vCol");
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(float) * 5, (void*) 0);
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+                          sizeof(float) * 5, (void*) (sizeof(float) * 2));
 
     return true;
 }
@@ -36,7 +94,25 @@ engine_t::terminate(){
 
 void
 engine_t::draw(){
+    glClearColor(0.8f, 0.3f, 0.7f, 1.0f);
+    float ratio;
+    int width, height;
+    
+    glfwGetFramebufferSize(window, &width, &height);
+    ratio = width / (float) height;
+    glViewport(0, 0, width, height);
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    glm::mat4 m(1.0f);
+    m = glm::rotate(m, (float) glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+    glm::mat4 p = glm::ortho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+    
+    glm::mat4 mvp = p * m;
+
+    glUseProgram(program);
+    glUniformMatrix4fv(mvp_location, 1, GL_FALSE, &mvp[0][0]);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 void
@@ -47,8 +123,12 @@ engine_t::update(double delta){
 
 void
 engine_t::run(){
+    double time = glfwGetTime();
     while (!glfwWindowShouldClose(window)){
         draw();
-        update(0.0);
+ 
+        double delta = glfwGetTime() - time;
+        update(delta);
+        time = glfwGetTime();
     }
 }
