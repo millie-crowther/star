75d0354e851aeaadfeb306723f13f887da6de757
millie <mbc15@ic.ac.uk>
2017-11-30 18:09:40 +0000

    got include of glfw working

--- installDependencies.sh
+++ installDependencies.sh
@@ -4,14 +4,14 @@ echo "+------------------+"
 sudo apt install cmake
 echo ""
 
-echo "+-----------------+"
-echo "| Installing SFML |"
-echo "+-----------------+"
-sudo apt-get install libsfml-dev
-echo ""
-
 echo "+----------------+"
 echo "| Installing GLM |"
 echo "+----------------+"
 sudo apt-get install libglm-dev
+echo ""
+
+echo "+-----------------+"
+echo "| Installing xorg |"
+echo "+-----------------+"
+sudo apt-get install xorg
 echo ""
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,9 @@
-#include <iostream>
 #include "engine.h"
-#include <SFML/Graphics.hpp>
 #include <glm/glm.hpp>
-#include "glm/gtc/matrix_transform.hpp"
+#include <GLFW/glfw3.h>
 
 int main(){
-  glm::mat4 myMatrix = glm::translate(glm::mat4(), glm::vec3(10.0f, 0.0f, 0.0f));
-  glm::vec4 myVector(10.0f, 10.0f, 10.0f, 0.0f);
-  glm::vec4 transformedVector = myMatrix * myVector; // guess the result
-
-  engine_t engine;
-  engine.run();
-
-
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
+    engine_t engine;
+    engine.run();
     return 0;
 }
