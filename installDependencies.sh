ea45a3d131d9c2ea61b0b206f0fde84e4565a4d4
millie <amelia.g.crowther@gmail.com>
2017-12-11 17:08:34 +0000

    added a few dependencies to install script and made cmakelists not hard link to glfw

--- installDependencies.sh
+++ installDependencies.sh
@@ -13,7 +13,7 @@ echo ""
 echo "+-----------------+"
 echo "| Installing xorg |"
 echo "+-----------------+"
-sudo apt-get install xorg
+sudo apt-get install xorg xorg-dev libglu1-mesa-dev
 echo ""
 
 echo "+-------------------+"
@@ -22,10 +22,36 @@ echo "+-------------------+"
 sudo apt-get install python
 echo ""
 
+echo "+----------------+"
+echo "| Installing pip |"
+echo "+----------------+"
+sudo apt install python-pip
+echo ""
+
 echo "+-----------------+"
 echo "| Installing glad |"
 echo "+-----------------+"
 sudo -H pip install glad
 cd build
-python -m glad --generator c --no-loader --out-path gladbuild 
-cd .. 
+if cd gladbuild; then
+    cd ..
+else
+    python -m glad --generator c --no-loader --out-path gladbuild 
+fi
+cd ..
+
+echo "+----------------+"
+echo "| Installing X11 |"
+echo "+----------------+"
+sudo apt-get install libx11-dev
+echo ""
+
+echo "+-----------------+"
+echo "| Installing GLFW |"
+echo "+-----------------+"
+if cd build/glfw; then 
+    git pull
+    cd ../..
+else
+    git clone https://github.com/glfw/glfw.git
+fi
